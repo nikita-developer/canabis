9de7b29a0b6e494e41db9f9de1225888c2cbe019
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-03-29 02:11:38 +0300

    заверстал десктоп

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,41 @@
 // $(document).ready(function() {
 //   console.log(1)
 // });
+
+
+
+    window.onload = function () {
+        var btn= document.getElementById('ryle__look'),
+        discount = document.getElementById('ryle__discount'),
+        ryle= document.getElementById('ryle'),
+        close= document.getElementById('ryle__close'),
+        box= document.getElementById('js-formRyle').getBoundingClientRect().top;
+        media= document.getElementById('ryle__media');
+
+        if (box<= 300) {
+            openRyle();
+        }
+
+        window.onscroll = function() {
+            box = document.getElementById('js-formRyle').getBoundingClientRect().top;
+            if (box<= 300) {
+                openRyle();
+            }
+        }
+
+        function openRyle() {
+            ryle.classList.add('ryle_active')
+            btn.onclick = function () {
+                media.classList.add('ryle__media_active');
+                setTimeout(function() {
+                    discount.classList.add('ryle__discount_active');
+                }, 5000);
+            }
+
+            close.onclick = function () {
+            ryle.classList.remove('ryle_active',setTimeout(function() {
+                ryle.parentNode.removeChild(ryle);
+                }, 1000));
+            }
+        }
+    }
